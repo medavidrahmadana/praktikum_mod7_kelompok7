ba6f5fa1694ad88e075d57074c24c3636800e670
PC9-IFLAB <Rskyfrhn801@gmail.com>
2023-04-05 17:25:19 +0700

    MENAMBAHKAN CLASS TEAMMEMBER

--- modul7_kelompok_07/modul7_kelompok_07/Program.cs
+++ modul7_kelompok_07/modul7_kelompok_07/Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 DataMahasiswa_13022130056 mahasiswa = DataMahasiswa_13022130056.ReadJSON("jurnal7_1_1302210056.json");
+TeamMembers_13022130056 dataAnggota = new TeamMembers_13022130056();
+dataAnggota.ReadJSON("jurnal7_2_13022130556.json");
