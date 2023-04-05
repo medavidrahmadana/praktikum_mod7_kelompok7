f89a557d64b50d54a2eb25563e54dbf3b42dad2f
unknown <gsherylaudreyy@gmail.com>
2023-04-05 17:22:56 +0700

    Menambahkan JSON 2
    Menambahkan class TeamMembers_1302213054

--- modul7_kelompok_07/modul7_kelompok_07/Program.cs
+++ modul7_kelompok_07/modul7_kelompok_07/Program.cs
@@ -7,6 +7,7 @@ class Program
     static void Main(string[] args)
     {
         DataMahasiswa_1302213054 mahasiswa = DataMahasiswa_1302213054.ReadJSON("jurnal7_1_1302213054.json");
-
+        TeamMembers_1302213054 dataAnggota = new TeamMembers_1302213054();
+        dataAnggota.ReadJSON("jurnal7_2_1302213054.json");
     }
 }
