3dbd7c3d23ae44afd38f24b3b9a18f3727c11583
PC9-IFLAB <ryunehaimu007@gmail.com>
2023-04-05 16:57:42 +0700

    Menambahkan class datamahasiswa_1302210056

--- modul7_kelompok_07/modul7_kelompok_07/Program.cs
+++ modul7_kelompok_07/modul7_kelompok_07/Program.cs
@@ -1,2 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+DataMahasiswa_13022130056 mahasiswa = DataMahasiswa_13022130056.ReadJSON("jurnal7_1_1302210056.json");
---
+++ modul7_kelompok_07/modul7_kelompok_07/Risky.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+
+class DataMahasiswa_13022130056
+{
+    public string firstName { get; set; }
+    public string lastName { get; set; }
+    public string gender { get; set; }
+    public int age { get; set; }
+    public Alamat address { get; set; }
+
+    public class Alamat
+    {
+        public string streetAddress { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+    }
+
+    public class Course
+    {
+        public string code { get; set; }
+        public string name { get; set; }
+    }
+
+    public List<Course> courses { get; set; }
+
+    public static DataMahasiswa_13022130056 ReadJSON(string fileName)
+    {
+        string jsonString = File.ReadAllText(fileName);
+        DataMahasiswa_13022130056 mahasiswa = JsonSerializer.Deserialize<DataMahasiswa_13022130056>(jsonString);
+
+        Console.WriteLine("Nama: {0} {1} \nGender: {2} \nBerumur: {3} \nBeralamat: {4}, {5}, {6}",
+            mahasiswa.firstName, mahasiswa.lastName, mahasiswa.gender, mahasiswa.age, mahasiswa.address.streetAddress, mahasiswa.address.city, mahasiswa.address.state);
+
+        Console.WriteLine("====================================================================");
+        Console.WriteLine("Daftar mata kuliah yang diambil:");
+        int i = 1;
+        foreach (Course course in mahasiswa.courses)
+        {
+            Console.WriteLine("MK {0} {1} - {2}", i++, course.code, course.name);
+        }
+        Console.WriteLine("====================================================================");
+
+        return mahasiswa;
+    }
+}
