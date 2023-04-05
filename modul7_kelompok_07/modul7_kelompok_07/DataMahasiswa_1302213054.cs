026c265a21918c26e9d83d788a981db312817736
unknown <you@example.com>
2023-04-05 16:55:00 +0700

    Menambahkan JSON Deserialization 1

---
+++ modul7_kelompok_07/modul7_kelompok_07/DataMahasiswa_1302213054.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+class DataMahasiswa_1302213054
+{
+    public string firstName { get; set; }
+    public string lastName { get; set; }
+    public string gender { get; set; }
+    public int age { get; set; }
+    public Alamat address { get; set; }
+
+    public class Alamat
+    {
+        public string streetAddress { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+    }
+
+    public class Course
+    {
+        public string code { get; set; }
+        public string name { get; set; }
+    }
+
+    public List<Course> courses { get; set; }
+
+    public static DataMahasiswa_1302213054 ReadJSON(string fileName)
+    {
+        string jsonString = File.ReadAllText(fileName);
+        DataMahasiswa_1302213054 mahasiswa = JsonSerializer.Deserialize<DataMahasiswa_1302213054>(jsonString);
+
+        Console.WriteLine("Nama: {0} {1} \nGender: {2} \nBerumur: {3} \nBeralamat: {4}, {5}, {6}",
+            mahasiswa.firstName, mahasiswa.lastName, mahasiswa.gender, mahasiswa.age, mahasiswa.address.streetAddress, mahasiswa.address.city, mahasiswa.address.state);
+
+        Console.WriteLine("====================================================================");
+        Console.WriteLine("Daftar mata kuliah yang diambil:");
+        int i = 1;
+        foreach (Course course in mahasiswa.courses)
+        {
+            Console.WriteLine("MK {0} {1} - {2}", i++, course.code, course.name);
+        }
+        Console.WriteLine("====================================================================");
+
+        return mahasiswa;
+    }
+}
--- modul7_kelompok_07/modul7_kelompok_07/Program.cs
+++ modul7_kelompok_07/modul7_kelompok_07/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataMahasiswa_1302213054 mahasiswa = DataMahasiswa_1302213054.ReadJSON("jurnal7_1_1302213054.json");
+
+    }
+}
