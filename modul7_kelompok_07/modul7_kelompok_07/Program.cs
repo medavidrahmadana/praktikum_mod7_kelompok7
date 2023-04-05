4f18eb74bffede82f2de82fd7adf24ae8154db13
PRAKTIKAN@0605-PC-21.(none)') <me.davidrahmadana@gmail.com>
2023-04-05 17:18:05 +0700

    MENAMBAHKAN JSON 2
    menambahkan class TeamMembers_1302213079

--- modul7_kelompok_07/modul7_kelompok_07/Program.cs
+++ modul7_kelompok_07/modul7_kelompok_07/Program.cs
@@ -7,7 +7,8 @@ class Program
     static void Main(string[] args)
     {
         DataMahasiswa_1302213079 mahasiswa = DataMahasiswa_1302213079.ReadJSON("jurnal7_1_1302213079.json");
-
+        TeamMembers_1302213079 dataAnggota = new TeamMembers_1302213079();
+        dataAnggota.ReadJSON("jurnal7_2_1302213079.json");
     }
 }
 
---
+++ modul7_kelompok_07/modul7_kelompok_07/TeamMembers_1302213079.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+class TeamMembers_1302213079
+{
+    public class Anggota
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string gender { get; set; }
+        public int age { get; set; }
+        public string nim { get; set; }
+    }
+
+    public List<Anggota> members { get; set; }
+
+    public void ReadJSON(string fileName)
+    {
+        string jsonString = File.ReadAllText(fileName);
+        TeamMembers_1302213079 dataAnggota = JsonSerializer.Deserialize<TeamMembers_1302213079>(jsonString);
+
+        Console.WriteLine("====================================================================");
+        Console.WriteLine("Daftar mata kuliah yang diambil:");
+        foreach (Anggota member in dataAnggota.members)
+        {
+            Console.WriteLine("{0} {1} {2} ( {3} - {4} )", member.nim, member.firstName, member.lastName, member.age, member.gender);
+        }
+        Console.WriteLine("====================================================================");
+    }
+}
+
