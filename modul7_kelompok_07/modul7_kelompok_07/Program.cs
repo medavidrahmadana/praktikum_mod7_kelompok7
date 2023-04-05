8d0d110b288d79f03f1b824d3959227975dcf9a5
PRAKTIKAN@0605-PC-21.(none)') <me.davidrahmadana@gmail.com>
2023-04-05 16:37:24 +0700

    MENAMBAHKAN JSON DESERIALIZATION 1
    menambahkan class DataMahasiswa_1302213079

--- modul7_kelompok_07/modul7_kelompok_07/Program.cs
+++ modul7_kelompok_07/modul7_kelompok_07/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataMahasiswa_1302213079 mahasiswa = DataMahasiswa_1302213079.ReadJSON("jurnal7_1_1302213079.json");
+
+    }
+}
+
