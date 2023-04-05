7359fd9c6e728e474633a9cff05b58877518ba17
PRAKTIKAN@0605-PC-21.(none) <mohamad.william24@gmail.com>
2023-04-05 17:11:37 +0700

    menambahkan class wili

--- modul7_kelompok_07/modul7_kelompok_07/Program.cs
+++ modul7_kelompok_07/modul7_kelompok_07/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataMahasiswa_1302213034 mahasiswa = DataMahasiswa_1302213034.ReadJSON("jurnal7_1_1302213034.json");
+
+    }
+}
